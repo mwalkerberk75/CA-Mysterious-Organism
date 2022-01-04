61894aa154721739c9a265fd19ea65a6361d12fe
mwalkerberk75 <71950586+mwalkerberk75@users.noreply.github.com>
2022-01-03 21:49:58 -0800

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,9 @@ function pAequorFactory (specimenNum, dna) {
           totalSimilar++;
         }
       }
-      console.log('Specimen', this.specimenNum, 'and specimen', newpAequor.specimenNum, 'have', ((totalSimilar/newpAequor.dna.length)*100).toFixed(2), '% DNA in common.');
+      const matchPerc = Number(((totalSimilar/newpAequor.dna.length)*100).toFixed(2));
+      // console.log('Specimen', this.specimenNum, 'and specimen', newpAequor.specimenNum, 'have',matchPerc,'% DNA in common.');
+      return matchPerc;
     },
     willLikelySurvive () {
       // Return true if 60% or more of the DNA is 'C' or 'G' 
@@ -45,8 +47,69 @@ function pAequorFactory (specimenNum, dna) {
         }
       }
       return (goodDNACount/this.dna.length >= 0.60);
+    },
+    complementStrand () {
+      // Generate complementary DNA strand 
+      // Substitutions: A -> T, T -> A, C -> G, G -> C
+      const newStrand = this.dna.map(dna => {
+        switch (dna) {
+          case 'A':
+            return 'T';
+            break;
+          case 'T':
+            return 'A';
+            break;
+          case 'G':
+            return 'C';
+            break;
+          case 'C':
+            return 'G';
+            break;
+        }
+      });
+      return newStrand;
+    },
+    findRelated (samplesArr, num) {
+      /* Find "num" instaces of pAequor most closely related to this instance
+         Accepts an array of pAequor samples for comparison
+         When searching, evaluates each entry in samplesArr by getting the match percentage
+         then comparing it to the current list of best matches. 
+         If the entry is found to be a better match, then it is inserted into the array
+         If the entry is not greater than any matches, it is inserted at the end of the array
+         After the better match comparison, the last entry is removed from the match Array
+      */   
+      let comparisonArr = []; // Will be used to store the best matches
+       for (let i = 0; i < samplesArr.length; i++) {
+        const obj = { // get specimen name and match % for comparison
+          specimenNum: samplesArr[i].specimenNum,
+          match: this.compareDNA(samplesArr[i]),
+          dna: samplesArr[i].dna
+        }
+        // Iterate over the existing comparison Array. If the existing specimen is a better match, insert into the comparison Array. 
+        let found = false;
+        let j = 0;
+        do {
+          if (j === comparisonArr.length) { // If less than the all matches in the array, add at the end and move on
+            comparisonArr[j] = obj;
+            found = true;
+          } else {
+            // If value is greater than the current index of the comparison array, insert at current location
+            // Inserting at the current location ensures that the comparison array is sorted by best to worst match
+            if (obj.match > comparisonArr[j].match && obj.specimenNum !== this.specimenNum) { 
+              comparisonArr.splice(j,0,obj); 
+              found = true;
+            }
+          }
+          j++;
+        } while (!found);
+        if (comparisonArr.length > num) { // Erase entries above the number needed
+              let k = comparisonArr.length - 1;
+              comparisonArr.pop();
+        }
+      }
+      return comparisonArr;
     }
-  }
+  } 
   return pAequor;
 }
 
@@ -66,13 +129,24 @@ function generatePopulation (num) {
 
 // Tests
 pAequorArray = generatePopulation(30) // Generate 30 instances of surviving pAequor
+console.log('Mutate test:');
 console.log(pAequorArray[14].specimenNum,pAequorArray[14].dna)
 pAequorArray[14].mutate();
 console.log(pAequorArray[14].specimenNum,pAequorArray[14].dna)
 console.log();
+console.log('Comparison test:');
 console.log(pAequorArray[10].specimenNum,pAequorArray[10].dna)
 console.log(pAequorArray[23].specimenNum,pAequorArray[23].dna)
-pAequorArray[10].compareDNA(pAequorArray[23]);
+console.log('Shared DNA:',pAequorArray[10].compareDNA(pAequorArray[23]));
 console.log();
+console.log('Survival test:')
 console.log(pAequorArray[4].specimenNum,pAequorArray[4].dna)
 console.log('Will likely survive?', pAequorArray[4].willLikelySurvive());
+console.log();
+console.log('Complement test');
+console.log(pAequorArray[29].specimenNum,pAequorArray[29].dna);
+console.log(pAequorArray[29].specimenNum,pAequorArray[29].complementStrand());
+console.log();
+console.log('Find related:');
+console.log(pAequorArray[7].specimenNum,pAequorArray[7].dna);
+console.log(pAequorArray[7].findRelated(pAequorArray, 5));
